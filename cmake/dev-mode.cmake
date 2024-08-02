8e86a4ffcd69ae546a2daf51415b89ba0b157973
Vladimir Zunin <vzunin@hse.ru>
2024-08-02 15:32:46 +0300

    Update use of easylogging++ library

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -1,5 +1,10 @@
 include(cmake/folders.cmake)
 
+include(CTest)
+if(BUILD_TESTING)
+  add_subdirectory(test)
+endif()
+
 option(BUILD_MCSS_DOCS "Build documentation using Doxygen and m.css" OFF)
 if(BUILD_MCSS_DOCS)
   include(cmake/docs.cmake)
