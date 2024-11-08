9553b618f9cc7296203843f96825b0cfa977b229
Fuuulkrum7 <ilka747428@gmail.com>
2024-11-08 17:18:31 +0300

    Add custom allocator

--- include/CircuitGenGraph/GraphMemory.hpp
+++ include/CircuitGenGraph/GraphMemory.hpp
@@ -1,25 +1,85 @@
 #pragma once
 
+#include <cassert>
 #include <memory_resource>
 #include <set>
 #include <string>
 #include <string_view>
 
+#define DEFAULT_BUF 81920
+#define CHUNK_SIZE 10240
+
 typedef unsigned char bytea;
 
-class LinearAllocator {
 
+/// @author Fuuulkrum7
+struct MultiLinearAllocator {
+  MultiLinearAllocator(size_t buf_size, size_t chunk_size)
+      : buf_size(buf_size)
+      , chunk_size(chunk_size) {
+    assert(buf_size >= 96);
+    assert(chunk_size >= 96);
+    blocks.reserve(5);
+    blocks.push_back(offset = new bytea[buf_size]);
+  }
+
+  ~MultiLinearAllocator() {
+    for (auto block: blocks) {
+      delete[] block;
+    }
+  }
+
+  template<typename T>
+  T* allocate() {
+    bytea *current = offset;
+    offset += sizeof(T);
+    align<T>();
+    if (offset > blocks.back() + buf_size) {
+      // bad alloc - memory was taken, but allocation is impossible, and we try
+      // to allocate more memory, than is located in buffer
+      if (offset == blocks.back()) {
+        return nullptr;
+      }
+      blocks.push_back(new bytea[chunk_size]);
+      offset = blocks.back();
+      return allocate<T>();
+    }
+    return reinterpret_cast<T*>(current);
+  }
+
+  void deallocate() {}
+
+private:
+  template<typename T>
+  void align() {
+    size_t suboffset = (uintptr_t)offset & (alignof(T) - 1);
+    if (suboffset) {
+      offset += (alignof(T) - suboffset);
+    }
+  }
+
+private:
+  std::vector<bytea *> blocks;
+  bytea* offset;
+  size_t buf_size;
+  size_t chunk_size;
 };
 
 /// @author Fuuulkrum7
 class GraphMemory {
 public:
-  /// @param initial_size size, which would be used for memory buffer reserve.
-  /// By default we allocate memory for 1024 verticies. Size of one vertex is
-  /// suuposed to be 64 bytes by default.
-  GraphMemory() : d_strings {&d_stringMemory} {
-    // TODO how to use buffer????
-  }
+  /// @param buf_size size, which would be used for memory buffer reserve.
+  /// By default we allocate memory for 1024 base verticies. Size of one vertex
+  /// is supposed to be 80 bytes by default.
+  /// @param chunk_size additional size, which would allocated, if buffer ends.
+  /// By default we allocate memory for 128 base verticies. Size of one vertex
+  /// is supposed to be 80 bytes by default.
+  GraphMemory(
+        size_t buf_size = DEFAULT_BUF,
+        size_t chunk_size = CHUNK_SIZE)
+      : d_vertexMemory(buf_size, chunk_size)
+      , d_strings {&d_stringMemory}
+  {}
 
   GraphMemory& operator=(GraphMemory&& other)      = delete;
   GraphMemory(GraphMemory&& other)                 = delete;
@@ -36,22 +96,12 @@ public:
 
   template<typename T>
   T* allocate() {
-    return static_cast<T*>(d_vertexMemory.allocate(sizeof(T), alignof(T)));
+    return d_vertexMemory.allocate<T>();
   }
 
-  // template<typename T>
-  // T* allocateForGraph() {
-  //   return static_cast<T*>(d_graphMemory.allocate(sizeof(T), alignof(T)));
-  // }
-
-  std::pmr::memory_resource& getVertexResource() { return d_vertexMemory; }
-
-  // std::pmr::memory_resource& getGraphResource() { return d_graphMemory; }
-
 private:
-  std::pmr::monotonic_buffer_resource d_vertexMemory;
-  // std::pmr::monotonic_buffer_resource d_graphMemory;
-  std::pmr::monotonic_buffer_resource d_stringMemory;
+  MultiLinearAllocator d_vertexMemory;
 
+  std::pmr::monotonic_buffer_resource d_stringMemory;
   std::pmr::set<std::string>          d_strings;
 };
