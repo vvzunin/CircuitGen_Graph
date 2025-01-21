5cd7cd848cbf5f8cf859c19c758156f3bf9008a4
Fuuulkrum7 <ilka747428@gmail.com>
2025-01-21 15:15:55 +0300

    Fix lint

--- test/src/MemoryManagement.cpp
+++ test/src/MemoryManagement.cpp
@@ -10,24 +10,24 @@
 #include "easylogging++Init.hpp"
 
 TEST(AllocatorTests, AllocateMethodTooBigStruct) {
-    MultiLinearAllocator allocator(196, 128);
-    struct BigStruct {
-        // will take 256 bytes
-        bytea many[256];
-    };
+  MultiLinearAllocator allocator(196, 128);
+  struct BigStruct {
+    // will take 256 bytes
+    bytea many[256];
+  };
 
-    BigStruct* ptr = allocator.allocate<BigStruct>();
-    CHECK_EQ(ptr, nullptr);
+  BigStruct *ptr = allocator.allocate<BigStruct>();
+  CHECK_EQ(ptr, nullptr);
 }
 TEST(AllocatorTests, AllocateMethodSmallStruct) {
-    MultiLinearAllocator allocator(196, 128);
-    struct SmallStruct {
-        // will take 196 bytes
-        bytea many[196];
-    };
+  MultiLinearAllocator allocator(196, 128);
+  struct SmallStruct {
+    // will take 196 bytes
+    bytea many[196];
+  };
 
-    SmallStruct* ptr1 = allocator.allocate<SmallStruct>();
-    CHECK_NE(ptr1, nullptr);
-    ptr1 = allocator.allocate<SmallStruct>();
-    CHECK_EQ(ptr1, nullptr);
+  SmallStruct *ptr1 = allocator.allocate<SmallStruct>();
+  CHECK_NE(ptr1, nullptr);
+  ptr1 = allocator.allocate<SmallStruct>();
+  CHECK_EQ(ptr1, nullptr);
 }
